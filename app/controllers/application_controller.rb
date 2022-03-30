4f9dd96cadfa34a6edc78d9ef6c4f3ecbe1c4393
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-30 08:22:40 -0400

    commit tree 4ddb19167603020dc1a2090861d54d5dd36befef

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < Sinatra::Base
+  # Add this line to set the Content-Type header for all responses
 
-  # add routes
+  #defining a route that responds to a `GET`
 
+  # request to `/bakeries` and returns an array of
+  # JSON objects for all bakeries in
+  # the database.
+
+  get '/bakeries' do
+    #get all the bakeries from the database
+    bakeries = Bakery.all
+
+    # return a JSON response with an array of all the bakery data
+    bakeries.to_json
+  end
 end
